14ebb27c7f654a277b5da369443a7aa5020979a9
yuribertoli <yuribertoli@yahoo.it>
2022-02-18 15:15:26 +0100

    title

---
+++ src/components/myTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myTitle'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
