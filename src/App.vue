c9c3ab0a7a735f978670be6064df0534876b75d9
yuribertoli <yuribertoli@yahoo.it>
2022-02-18 15:27:39 +0100

    hero

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <myTitle></myTitle>
+    <newHero></newHero>
   </div>
 </template>
 
 <script>
 import myTitle from './components/myTitle.vue'
+import newHero from './components/newHero.vue'
 
 export default {
   name: 'App',
   components: {
-    myTitle
+    myTitle,
+    newHero
   }
 }
 </script>
---
+++ src/components/newHero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="jumbotron">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newHero'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.jumbotron {
+  height: 300px;
+  border: 1px solid black;
+  width: 80%;
+  margin: 0 auto;
+}
+
+</style>
